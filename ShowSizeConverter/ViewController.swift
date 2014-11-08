62eb0c8da18908ee3f0e143382ce5af390417bcc
Arthur Labisi Siloka <Silokas_Mac@Arthurs-MacBook-Pro.local>
2014-11-09 00:05:39 +0800

    first code

--- ShowSizeConverter/ViewController.swift
+++ ShowSizeConverter/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
+    
+    @IBOutlet var mensConvertedShoeSizeLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +25,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        let sizeFromTextField = mensShoeSizeTextField.text
+        let numberFromTextField = sizeFromTextField.toInt()
+        var integerFromTextField = numberFromTextField!
+        let conversionConstant = 30
+        integerFromTextField += conversionConstant
+        mensConvertedShoeSizeLabel.hidden = false
+        let stringWithUpdstedShoeSize = "\(integerFromTextField)"
+        mensConvertedShoeSizeLabel.text = stringWithUpdstedShoeSize
+        
+    }
 
 }
 
